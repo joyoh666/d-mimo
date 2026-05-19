e94b5b1809685cf34b6f94892e81a71b20faa439
joyoh666 <joeyoh666@gmail.com>
2026-05-19 15:51:42 +0900

    5/19

--- RT_intro.py
+++
@@ -1,8 +0,0 @@
-import sionna.rt
-import matplotlib.pyplot as plt
-import mitsuba as mi
-import numpy as np
-from sionna.rt import load_scene, PlanarArray, Transmitter, Receiver, Camera,\
-                      PathSolver, RadioMapSolver, subcarrier_frequencies
-
-scene = load_scene(sionna.rt.scene.munich)
